9b06a5ca901c0d38f7e4667822428aab3fcd5156
bo-crandall <23106933+bo-crandall@users.noreply.github.com>
2022-12-11 11:19:09 -0500

    discord ingest first pass working

--- ingest.py
+++ ingest.py
@@ -1,6 +1,9 @@
 import discord
 import mysql.connector as mysql
 import re
+import string
+
+optoutid = 1051237774130417684
 
 dbc = mysql.connect(
     host = "delphi",
@@ -38,8 +41,11 @@ async def on_message(message):
         channel = message.channel
         for i in guild.text_channels:
             #await i.send("#I am scanning this channel")
-            async for m in i.history(limit=10,oldest_first=False):
-                text = str(m.clean_content).encode('ascii','ignore').decode() #cast content to string and convert it to ascii only, ignoring any characters that don't fit.
+            messages = 0
+            async for m in i.history(limit=None):
+                messages += 1
+                if messages % 10 == 0:
+                    print(messages)
                 #start filtering unsuitable messages, we don't want attachments, embeds,  or stickers 
                 if len(m.attachments) > 0:
                     continue
@@ -47,6 +53,11 @@ async def on_message(message):
                     continue
                 if len(m.stickers) > 0:
                     continue
+                text = m.clean_content.encode('ascii','ignore').decode() #convert to ascii only, ignoring any characters that don't fit.
+                if text[0] not in ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@"'): #ignore any messages starting with funky characters.  Filters bot commands
+                    continue
+                text = re.sub(r'(<:)(.*)>',' ',text) #filter customer emojis.  Discord uses in-band signaling for custom emojis, so we just replace that signal with nothing
+                #print("Message:",text)
                 text = re.sub(r'[^a-zA-Z0-9 ]','',text.lower().strip()) #simplify the string
                 stext = text.split() #split the text into words
                 dbc.start_transaction()
@@ -61,15 +72,19 @@ async def on_message(message):
                     else:
                         first = stext[w]
                         next = stext[w+1]
-                    sql.execute("select count(*) from single where first = %s and next = %s",(first,next))
+                    sql.execute("select weight from single where first = %s and next = %s",(first,next))
                     dbresult = sql.fetchall()
-                    if dbresult[0][0] == 0:
+                    #print("DBResult:",dbresult)
+                    if len(dbresult) == 0: #if there are zero occurances of that first and next, make a new one
                         sql.execute("insert into single (first, next, weight) values (%s, %s, 1)",(first, next))
-                        print("Insert", first, next)
-                    else:
-                        sql.execute("update single set weight = weight + 1")
-                        print("Update", first, next)
+                    else: #if it does already exist, update the weight
+                        newweight = dbresult[0][0] + 1
+                        #print("New Weight:",newweight)
+                        sql.execute("update single set weight = %s where first = %s and next = %s",(newweight, first, next))
+
                 dbc.commit()
+    if message.content.startswith("$emote"):
+        await message.channel.send("<:rooThink1:580614815408586762>")
 
                         
 
